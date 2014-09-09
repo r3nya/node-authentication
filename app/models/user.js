4000c23200526aa13cbe3f3a4369285f7a28638d
Andrew M <me@r3nya.ru>
2014-09-09 22:08:55 +0400

    fix

--- app/models/user.js
+++ app/models/user.js
@@ -16,7 +16,7 @@ userSchema.methods.generateHash = function(password) {
 
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password):
+    return bcrypt.compareSync(password, this.local.password);
 };
 
 // create the model for users
